8694afca24df8ce56197d3ef878d91259e42dffc
Adrián Ricardo Iazbeck Scalia <adrian.r.scalia@gmail.com>
2023-05-03 18:02:10 -0300

    alter test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Um + para Jesus App',
+      title: 'Um + para Jesus',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
